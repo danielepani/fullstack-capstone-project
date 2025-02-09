654b370384660ea38bfafa9a8e111d11274832f0
danielepani <daniele.pani.86@gmail.com>
2025-02-09 15:24:34 -0500

    Created Register & LOgin

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -16,6 +16,8 @@ function App() {
           {/* the final code will not pass the products to every page, but each page will call the server API */}
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
+          <Route path="/app/login" element={<LoginPage/>} />
+          <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/search" element={<SearchPage />} />
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
         </Routes>
